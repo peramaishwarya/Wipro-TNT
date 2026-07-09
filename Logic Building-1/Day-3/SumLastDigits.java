a26419ae44a2f37f04014bd80c333ae3c8d9c781
peramaishwarya <peramaishwarya1118@gmail.com>
2026-07-09 22:40:49 +0530

    Add files via upload

---
+++ Logic Building-1/Day-3/SumLastDigits.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class SumLastDigits {
+    public static int addLastDigits(int input1, int input2) {
+        int last1 = Math.abs(input1 % 10);
+        int last2 = Math.abs(input2 % 10);
+        return last1 + last2;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+        System.out.println("Sum of Last Digits = " + addLastDigits(a, b));
+        sc.close();
+    }
+}
